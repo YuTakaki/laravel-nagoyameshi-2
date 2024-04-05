2be09d5c0204a0d512059ec4534596870fd939b8
YuTakaki <yu.seattle.0215@icloud.com>
2024-04-06 05:54:07 +0900

    feature-admin-restaurants

---
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Restaurant;
+
+class RestaurantController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    public function index(Request $request)
+    {
+        $keyword = $request->keyword;
+
+         if ($keyword !== null) {
+            $restaurants = Restaurant::where('name', 'like', "%{$keyword}%")->paginate(15);
+            $total = $restaurants->total();
+         } else {
+            $restaurants = Restaurant::paginate(15);
+            $total = Restaurant::all()->count();
+         }
+
+        return view('admin.restaurants.index', compact('restaurants', 'total', 'keyword'));
+    }
+
+    public function show(Restaurant $restaurant)
+    {
+        return view('admin.restaurants.show', compact('restaurant'));
+    }
+
+    public function create(Restaurant $restaurant)
+    {
+        return view('admin.restaurants.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'image' => 'image|max:2048',
+            'description' => 'required',
+            'lowest_price' => 'required|numeric|min:0|lte:highest_price',
+            'highest_price' => 'required|numeric|min:0|gte:lowest_price',
+            'postal_code' => 'required|digits:7',
+            'address' => 'required',
+            'opening_time' => 'required|before:closing_time',
+            'closing_time' => 'required|after:opening_time',
+            'seating_capacity' => 'required|numeric|min:0'
+        ]);
+
+        $restaurant = new Restaurant();
+        $restaurant->name = $request->input('name');
+        $restaurant->description = $request->input('description');
+        $restaurant->lowest_price = $request->input('lowest_price');
+        $restaurant->highest_price = $request->input('highest_price');
+        $restaurant->postal_code = $request->input('postal_code');
+        $restaurant->address = $request->input('address');
+        $restaurant->opening_time = $request->input('opening_time');
+        $restaurant->closing_time = $request->input('closing_time');
+        $restaurant->seating_capacity = $request->input('seating_capacity');
+
+        if($request->hasFile('image')) {
+            $image = $request->file('image')->store('public/restaurants');
+            $restaurant->image_name = basename($image);
+        } else {
+            $restaurant->image = '';
+        }
+
+        $restaurant->save();
+
+        return view('admin.restaurants.index', compact('restaurant'))->with('flash_message', '店舗を登録しました。');
+    }
+
+    public function edit(Restaurant $restaurant)
+    {
+        return view('admin.restaurants.edit', compact('restaurant'));
+    }
+
+    public function update(Restaurant $restaurant)
+    {
+        $request->validate([
+            'name' => 'required',
+            'image' => 'image|max:2048',
+            'description' => 'required',
+            'lowest_price' => 'required|numeric|min:0|lte:highest_price',
+            'highest_price' => 'required|numeric|min:0|gte:lowest_price',
+            'postal_code' => 'required|digits:7',
+            'address' => 'required',
+            'opening_time' => 'required|before:closing_time',
+            'closing_time' => 'required|after:opening_time',
+            'seating_capacity' => 'required|numeric|min:0'
+        ]);
+
+        $restaurant->name = $request->input('name');
+        $restaurant->description = $request->input('description');
+        $restaurant->lowest_price = $request->input('lowest_price');
+        $restaurant->highest_price = $request->input('highest_price');
+        $restaurant->postal_code = $request->input('postal_code');
+        $restaurant->address = $request->input('address');
+        $restaurant->opening_time = $request->input('opening_time');
+        $restaurant->closing_time = $request->input('closing_time');
+        $restaurant->seating_capacity = $request->input('seating_capacity');
+
+        if($request->hasFile('image')) {
+            $image = $request->file('image')->store('public/restaurants');
+            $restaurant->image = basename($image_path);
+        }
+
+        return view('admin.restaurants.index', compact('restaurant'))->with('flash_message', '店舗を編集しました。');
+
+    }
+
+    public function destroy(Restaurant $restaurant)
+    {
+        $restaurant->delete();
+
+        return view('admin.restaurants.index', compact('restaurant'))->with('flash_message', '店舗を削除しました。');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
+use App\Http\Controllers\Admin\RestaurantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,16 @@ require __DIR__.'/auth.php';
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
 });
+
+Route::controller(RestaurantController::class)->group(function () {
+    Route::get('/admin/restaurants/index', 'index')->name('admin.restaurants.index');
+    Route::get('/admin/restaurants/show/{restaurant}', 'show')->name('admin.restaurants.show');
+    Route::get('/admin/restaurants/edit/{restaurant}', 'edit')->name('admin.restaurants.edit');
+    Route::get('/admin/restaurants/create', 'create')->name('admin.restaurants.create');
+    Route::post('/admin/restaurants/destroy', 'index')->name('admin.restaurants.destroy');
+    Route::post('/admin/restaurants/update', 'index')->name('admin.restaurants.update');
+  });
+
+
+
+
